ffd77b5ee33d5fd3dc7947b5445e5087bfc5b448
evey <zhongwen322@gmail.com>
2016-12-30 11:36:51 +1300

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,4 +7,17 @@ def new
   @group = Group.new
 end
 
+#此处是实做new里表单送出的信息
+def create
+  @group = Group.new(group_params)
+  @group.save
+  redirect_to groups_path
+end
+private
+def group_params
+  params.require(:group).permit(:title, :description)
+end
+#此处是实做new里表单送出的信息---
+
+
 end
